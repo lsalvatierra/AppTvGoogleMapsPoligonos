75a061a55940fa596615d2d37568e156f94786a7
LuisAngel <luisangel.salvat@gmail.com>
2021-05-31 19:28:18 -0500

    actualización

--- app/src/main/java/com/qbo/apptvgooglemapspoligonos/MapsActivity.kt
+++ app/src/main/java/com/qbo/apptvgooglemapspoligonos/MapsActivity.kt
@@ -42,7 +42,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPolygo
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(-12.115896, -77.033486)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         //Habilitar los controles de Zoom
